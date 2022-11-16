a6d427032240fbdebe633e629c19a3d8fb1af53c
mochi-666 <94042218+mochi-666@users.noreply.github.com>
2022-11-16 22:09:12 +0900

    first commit

---
+++ 練習1/src/練習1/練習1の2.java
@@ -0,0 +1,11 @@
+package 練習1;
+
+public class 練習1の2 {
+  public static void main (String[] args) {
+    boolean result = true;
+    char a = '駆';
+    double b = 3.14;
+    long number = 314159265853979L;
+    String c = "ミナトの攻撃！　敵に１５ポイントのダメージを与えた。";
+  }
+}
