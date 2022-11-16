a017579622f82a8180652a81a13cc2453e895bdb
mochi-666 <94042218+mochi-666@users.noreply.github.com>
2022-11-16 22:55:34 +0900

    update

---
+++ 練習2/src/練習2/テキスト2の1.java
@@ -0,0 +1,12 @@
+package 練習2;
+
+public class テキスト2の1 {
+  public static void main(String[] args) {
+	int a;
+	int b;
+	a = 20;
+	b = a + 5;
+	System.out.println(a);
+	System.out.println(b);
+  }
+}
---
+++ 練習2/src/練習2/テキストコード2の2.java
@@ -0,0 +1,7 @@
+package 練習2;
+
+public class テキストコード2の2 {
+  public static void main(String[] args) {
+	  System.out.println("私の好きな記号は二重引用符 （\") です");
+  }
+}
